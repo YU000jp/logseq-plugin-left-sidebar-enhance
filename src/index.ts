e9ee4a7fec0d004a1efea128ff6dfd8537e55f4d
YU <111847207+YU000jp@users.noreply.github.com>
2024-01-07 16:42:04 +0900

    refactor(Remove): (New Page as sub page)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import '@logseq/libs' //https://plugins-doc.logseq.com/
-import { settingsTemplate } from './settings'
-import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
-import ja from "./translations/ja.json"
-import { loadShowByMouseOver } from './mouseover'
+import '@logseq/libs'; //https://plugins-doc.logseq.com/
+import { setup as l10nSetup } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
 import { loadDateSelector } from './dateSelector'
-//import { loadNewChildPageButton } from "./newChildPageButton";
+import { loadShowByMouseOver } from './mouseover'
+import { settingsTemplate } from './settings'
+import ja from "./translations/ja.json"
 
 /* main */
 const main = async () => {
@@ -19,10 +18,6 @@ const main = async () => {
   //日付セレクター
   loadDateSelector()
 
-  // //サブページ作成補助 (現在のページ名と/を入力済みにする)
-  // loadNewChildPageButton();
-
 }/* end_main */
-
 
 logseq.ready(main).catch(console.error)
